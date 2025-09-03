a3db6085846fc518f972f69b87c888a2220f0741
hanyuseung <hanyusmail0707@gmail.com>
2025-09-03 22:47:55 +0900

    15678 done

--- SINCHON/w3/2098.cpp
+++ SINCHON/w3/2098.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
+
 
 #define FASTIO\
 ios::sync_with_stdio(false);\
@@ -11,22 +13,33 @@ cout.tie(NULL);
 
 typedef long long ll;
 
+enum {UP, LEFT, DOWN, RIGHT};
+
 void solve(){
     int n; cin >> n;
-    vector<vector<int>> W (n+1, vector<int>(n+1));
-    vector<vector<bool>> visited(n+1, vector<bool>(n+1));
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            cin >> W[i][j];
-            visited[i][j] = false;
+    vector<vector<int>> W (n+2, vector<int>(n+1));
+    vector<vector<bool>> visited(n+2, vector<bool>(n+2));
+    for(int i = 0; i <= n+1; i++){
+        for(int j = 0; j <= n+1; j++){
+            if(i == 0 || j == 0 || i == n + 1 || j == n + 1 ){
+                W[i][j] = INT_MAX;
+            } else {
+                cin >> W[i][j];
+                visited[i][j] = false;
+            }
         }
     }
-   vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(4)));
-   
-
+    vector<vector<vector<int>>> dp(n+2, vector<vector<int>>(n+2, vector<int>(4, 0)));
+    for(int i = 0; i < 4; i++) dp[1][1][i] = W[1][1];
 
     
-    // f(i,j,d) 해서 d 는 direction -> 0,1,2,3 상하좌우 하고
+    
+    // f(cur, bit) = 현재 정점이 cur, visited 가 true일때 min.
+    // f(cur, bit) = min(dist(cur, next) + f(next, bit + next)).
+
+   
+    
+    // f(i,j,d) 해서 d 는 direction -> 0,1,2,3 wasd
     // 구석에 박혀있거나 벽에 붙어있는 것들은 초기에 max처리
     // visited [][] 로 방문된거 재방문 안하게 처리.
 }
--- SINCHON/w4/15678.cpp
+++ SINCHON/w4/15678.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <limits.h>
 #define FASTIO \
 cin.tie(NULL); \
 cout.tie(NULL); \
@@ -21,15 +22,68 @@ typedef long long ll;
  * i-D ~ i 까지중 최대인 D[j] 구하기 -> D[i]를 x로 업데이트
 */
 
+vector <ll> node;
+vector <ll> arr;
+vector <ll> D;
+
+ll max2(ll a, ll b){
+    return a > b ? a:b;
+}
+
+
+ll update(int i, ll x, int n, int l, int r){
+    if(l >= i && r <= i) return node[n] = x;
+    if(l > i || r < i) return node[n];
+    int m = (l + r)/2;
+    const ll lval = update(i, x, 2*n, l, m);
+    const ll rval = update(i, x, 2*n+1, m+1, r);
+    return node[n] = max2(lval, rval);
+}
+
+ll query(int qs, int qe, int n, int s, int e){
+    if (e < qs || qe < s) return LONG_LONG_MIN;
+    if (qs <= s && e <= qe) return node[n];
+
+    int m = (s+e)/2;
+    const ll lval = query(qs, qe, 2*n, s, m);
+    const ll rval = query(qs, qe, 2*n+1, m+1, e);
+    return max2(lval, rval);
+}
+
+
+
+
 void solve(){
     int n,d; cin >> n >> d;
-    ll k;
-    for(int i = 0; i < n; i++){
-        cin >> k;
+    arr.assign(n+1, 0);
+    D.assign(n+1,LONG_LONG_MIN);
+    node.assign(4*n+4, LONG_LONG_MIN);
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
     }
+
+    D[1] = arr[1];
+    update(1, D[1], 1, 1, n);
+    // dp
+    ll maxn = D[1];
+    for(int i = 2; i <= n; i++){
+        // d가 얼마일지 모르니까
+        int left = max(1, i - d), right = i-1;
+
+        ll best = LONG_LONG_MIN;
+        if(left <= right) best = query(left, right , 1,1,n);
+
+        if(best == LONG_LONG_MIN) D[i] = arr[i];
+        else D[i] = max2(arr[i], best + arr[i]);
+
+        update(i, D[i], 1, 1, n);
+        if(maxn < D[i]) maxn = D[i];
+    }
+    cout << maxn << "\n";
 }
 
 int main(void){
+    FASTIO
     int t = 1;
     for(int i = 0; i< t; i++) solve();
     return 0;
